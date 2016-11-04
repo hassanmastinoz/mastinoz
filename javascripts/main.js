591d9fb569380ac8b87a4e8d207fcf901ff9f84c
mastinoz <persiste1@gmail.com>
2016-11-04 13:14:43 +0000

    Register A service worker

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,10 @@
 console.log('This would be the main JS file.');
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
